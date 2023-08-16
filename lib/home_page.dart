e4fb603b39108ec5404de1f6d94e6533266135b8
Inshal Ahmad <inshal.ahamed@diversey.com>
2023-08-16 19:29:49 +0530

    added all files

---
+++ lib/home_page.dart
@@ -0,0 +1,209 @@
+import 'dart:convert';
+
+import 'package:assignment_1/model/data_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Model model;
+  List<Results> list = [];
+
+
+  // void listItem(int index){
+  //   final data = _data[index];
+  //
+  // }
+
+
+  // Future fetchData() async {
+  //   final url = await http.get(Uri.parse("https://randomuser.me/api/?results=5"));
+  //   model = Model.fromJson(jsonDecode(url.body));
+  //   setState(() {
+  //     list = model.results!;
+  //   });
+  // }
+
+  Future<List<dynamic>> fetchUsers() async {
+    var result = await http.get(
+        Uri.parse("https://randomuser.me/api/?results=5"));
+    return json.decode(result.body)['results'];
+  }
+
+  String _name(dynamic user) {
+    return user['name']['title'] + " " + user['name']['first'] + " " +
+        user['name']['last'];
+  }
+
+  String _location(dynamic user) {
+    return user['location']['country'];
+  }
+
+  String _email(Map<dynamic, dynamic> user) {
+    return user['email'];
+  }
+
+
+  String _dob(Map<dynamic, dynamic> user) {
+    String dob = user['dob'].toString().split("T")[0];
+    String dob2 = dob.split(":")[1];
+    return dob2;
+  }
+
+  String _uuid (Map<dynamic, dynamic> user) {
+    return user['login']['uuid'].toString().split("-")[0];
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // fetchData();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Color(0xffECECEC),
+          title: Text("Random Users",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.black,fontSize: 17,),),
+          centerTitle: true,
+        ),
+        body: FutureBuilder<List<dynamic>>(
+          future: fetchUsers(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              // print(_email(snapshot.data[0]));
+              return ListView.builder(
+                  padding: EdgeInsets.all(15),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Container(
+                              height: 155,width: 112,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(image:NetworkImage(snapshot.data[index]['picture']['large']),
+                                    fit:BoxFit.fill ),
+                                shape: BoxShape.rectangle
+                              ),
+                              // child: Image(image: NetworkImage(snapshot.data[index]['picture']['large'],),
+                              //   height: 170,width: 130,fit: BoxFit.cover,),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                 Padding(
+                                   padding: EdgeInsets.only(left: 5,bottom: 5),
+                                   child: Text(_name(snapshot.data[index]),style:TextStyle(fontSize:18,fontWeight: FontWeight.bold,color: Colors.black),),
+                                 ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 5,bottom: 5),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text("Country ",style:TextStyle(fontSize: 8,color: Colors.grey)),
+                                      Text(_location(snapshot.data[index]),style:TextStyle(fontSize: 15)),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding:EdgeInsets.only(left: 5,bottom: 5),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text("Email ",style:TextStyle(fontSize: 8,color: Colors.grey)),
+                                      Text(_email(snapshot.data[index]),style:TextStyle(fontSize: 15)),
+                                    ],
+                                  ),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 5,bottom: 0),
+                                      child: Text("D.O.B. ",style:TextStyle(fontSize: 8,color: Colors.grey)),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(bottom: 5),
+                                      child: Text(_dob(snapshot.data[index]),style:TextStyle(fontSize: 15)),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 5,bottom: 5),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text("ID Number ",style:TextStyle(fontSize: 8,color: Colors.grey)),
+                                      Text(_uuid(snapshot.data[index]).toUpperCase(),style:TextStyle(fontSize: 15)),
+                                    ],
+                                  ),
+                                ),
+                                Divider(thickness: 1,color: Colors.grey),
+                              ],
+                            ),
+                          ),
+
+
+                          // Text(_name(snapshot.data[index]),
+                          //   style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),),
+                    // Card(
+                    //   child: Column(
+                    //     children: <Widget>[
+                    //       ListTile(
+                    //         leading: Text(_name(snapshot.data[index])),
+                    //         title: Text(_location(snapshot.data[index])),
+                    //         subtitle: Text(_email(snapshot.data[index])),
+                    //         trailing: Text(_dob(snapshot.data[index])),
+                    //       )
+                    //     ],
+                    //   ),
+                    // ),
+
+                        ],
+                      );
+
+                      // Card(
+                      //   child: Column(
+                      //     children: <Widget>[
+                      //       ListTile(
+                      //         leading: CircleAvatar(
+                      //             radius: 30,
+                      //             backgroundImage: NetworkImage(snapshot
+                      //                 .data[index]['picture']['large'])),
+                      //         title: Text(_name(snapshot.data[index])),
+                      //         subtitle: Text(_location(snapshot.data[index])),
+                      //         trailing: Text(_email(snapshot.data[index])),
+                      //       )
+                      //     ],
+                      //   ),
+                      // );
+                  });
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+
+
+        )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:assignment_1/home_page.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+
+}
+
+
+
